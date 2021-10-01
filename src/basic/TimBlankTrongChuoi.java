382bd8c22a68d0bc6b6c54e644200259e846eedf
quynhtq267 <quynhtq267@gmail.com>
2021-10-01 18:13:43 +0700

    Check palindrome text

--- src/basic/TimBlankTrongChuoi.java
+++ src/basic/TimBlankTrongChuoi.java
@@ -1,9 +1,12 @@
 package basic;
 import static java.lang.Character.isWhitespace;
 //Nếu chỗ này ko import static method thì phải viết Character.isWhitespace
+//Để gọi tới 1 method ở khác class thì: 1/Nếu method = static then Class.method hoặc import method; 2/Nếu method != static then khởi tạo bien instance cho class đó rồi gọi bien.method
+
 
 public class TimBlankTrongChuoi {
 	public static void main(String[] args) {
+		//Neu viet ben trong method thi phai khoi tao gia tri ban dau, con viet ben ngoai method thi ko can
 			int count=0;
 		String text = "This code appears bit shorter than the traditional approach of checking the returned value directly for";
 		for (int i = 0; i < text.length(); i++) {
@@ -13,5 +16,6 @@ public class TimBlankTrongChuoi {
 			}
 		}
 		System.out.println(count);
+
 	}
 }
---
+++ src/basic/checkPalindromeText.java
@@ -0,0 +1,28 @@
+package basic;
+
+public class checkPalindromeText {
+	public static void main(String[] args) {
+		checkPalindromeText t = new checkPalindromeText();
+		System.out.println(t.isPalindrome2("wow"));
+	}
+
+	public boolean isPalindrome1(String text) {
+		for (int i = 0; i < text.length() / 2; i++) {
+			if (text.charAt(i) == text.charAt(text.length() - 1 - i)) {
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	
+//equals chỉ so sánh
+	public boolean isPalindrome2(String text) {
+		StringBuilder str = new StringBuilder(text);
+		if (text.equals(str.reverse().toString())){
+			return true;
+		}
+		return false;
+	}
+}
